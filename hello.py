63b4414f0c800944c4c151d34a9094482a9a194e
Kedrin Viktor <kedrinvs@mail.ru>
2020-11-28 16:29:41 +0000

    step gunicorn

---
+++ hello.py
@@ -0,0 +1,8 @@
+def app(environ, start_response):
+    status = '200 OK'
+    response_headers = [
+    ('Content-type', 'text/plain')
+    ]
+    start_response(status, response_headers)
+    body =  [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
+    return body
