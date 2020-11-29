6d88a8ab47190841ade7bc1ed257774d88965a2a
box <box@MariRyzen.localdomain>
2020-11-29 21:03:14 +0800

    django project

--- init2.sh
+++ init2.sh
@@ -1,4 +1,5 @@
 sudo ln -sf /home/box/web/etc/myweb.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-gunicorn --bind 0.0.0.0:8080 hello:app
+cd /home/box/web/ask/
+gunicorn -c /home/box/web/django_conf.py ask.wsgi:application
