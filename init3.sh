10737793d9ccf32ca639ec10ef35ba6845855497
box <box@MariRyzen.localdomain>
2020-12-02 00:02:35 +0800

    django mysql 3

--- init3.sh
+++ init3.sh
@@ -1,7 +1,6 @@
 sudo ln -sf /home/box/web/etc/myweb.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-
-sudo ln -sf /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
-sudo ln -sf /home/box/web/etc/djangoconf.py /etc/gunicorn.d/djangoconf.py
+sudo ln -sf /home/box/web/etc/hello.py  /etc/gunicorn.d/test-hello
+sudo ln -sf /home/box/web/etc/djangoconf.py /etc/gunicorn.d/test-django
 sudo /etc/init.d/gunicorn restart
--- initdb.sh
+++ initdb.sh
@@ -4,5 +4,4 @@ sudo /etc/init.d/mysql start
 mysql -uroot -e "CREATE USER 'box'@'localhost' IDENTIFIED BY 'box123'"
 mysql -uroot -e "CREATE DATABASE stepik"
 mysql -uroot -e "GRANT ALL ON stepik.* TO 'box'@'localhost'"
-python ~/web/ask/manage.py makemigrations
-python ~/web/ask/manage.py migrate
+mysql -uroot -e "FLUSH PRIVILEGES;"
